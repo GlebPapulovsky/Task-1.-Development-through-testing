4526fdfb5ef5a3fc3f8d644a515b8307ed1f2d0b
GlebPapulovsky <papulovskijglech@gmail.com>
2024-09-21 23:47:58 +0300

    findByNumber implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -4,12 +4,17 @@ import java.util.HashMap;
 
 public class PhoneBook {
     HashMap<String,String>map=new HashMap<>();
+
+
     public int add(String name,String number){
         if(!map.containsKey(name)){
             map.put(name,number);
             return map.size();
         }
         return 0;
+    }
+    public String findByNumber(String number){
+        return "";
     }
 
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -9,4 +9,11 @@ public class PhoneBookTest {
         PhoneBook phoneBook = new PhoneBook();
         Assert.assertEquals(1, phoneBook.add("alex", "11111111111"));
     }
+    @Test
+    public void findByNumberTest(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("alex", "11111111111");
+        Assert.assertEquals("alex",phoneBook.findByNumber("11111111111"));
+    }
+
 }
